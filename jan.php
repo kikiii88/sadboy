4709d7b9ededbe63fe52f5bf59edc0c0b4bbd85e
Kiki <kikiamalia122000@gmail.com>
2024-08-04 19:49:57 +0700

    Create jan.php

---
+++ jan.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en-us" class="no-js">
+
+	<head>
+		<meta charset="utf-8">
+        <title>We are Coming Soon - Hosted by XeonBD</title>
+        <meta name="description" content="This website is hosted by XeonBD, one of the best & reliable premium web hosting service provider.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="XeonBD">
+
+        <!-- ================= Favicon ================== -->
+        <!-- Standard -->
+        <link rel="shortcut icon" href="http://www.xeonbd.com/images/favicon.ico">
+        <!-- Retina iPad Touch Icon-->
+        <link rel="apple-touch-icon" sizes="144x144" href="http://www.xeonbd.com/images/favicon.ico">
+        <!-- Retina iPhone Touch Icon-->
+        <link rel="apple-touch-icon" sizes="114x114" href="http://www.xeonbd.com/images/favicon.ico">
+        <!-- Standard iPad Touch Icon--> 
+        <link rel="apple-touch-icon" sizes="72x72" href="http://www.xeonbd.com/images/favicon.ico">
+        <!-- Standard iPhone Touch Icon--> 
+        <link rel="apple-touch-icon" sizes="57x57" href="ihttp://www.xeonbd.com/images/favicon.ico">
+
+        <!-- ============== Resources style ============== -->
+        <link rel="stylesheet" href="css/style-minimal.css" />
+
+		<!-- Modernizr runs quickly on page load to detect features -->
+		<script src="js/modernizr.custom.js"></script>
+	</head>
+	
+	<body>
+
+		<!-- Page preloader -->
+		<div id="loading">
+			<div id="preloader">
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+
+		<!-- Overlay -->
+		<div class="global-overlay">
+			<div class="overlay skew-part"></div>
+		</div>
+
+		<!-- START - Home/Left Part -->
+		<section id="left-side" class="minimal-phone">
+
+			<!-- Your logo -->
+			<img src="img/XeonBD.png" alt="XeonBD" class="brand-logo" />
+
+			<div class="content">
+<br /><br /><br />
+				<h1 class="text-intro opacity-0">We are coming soon
+				</h1>
+
+				<h2 class="text-intro opacity-0">and Yes! we are proudly powered by <a href="http://www.xeonbd.com/">XeonBD</a><br /></h2>
+
+<br />
+XeonBD<br />
+Suite 2A, House 231 (KAZI BARI), Road 06 <br />
+Mohammadi Housing Limited, Mohammadpur <br />
+Dhaka - 1207, Bangladesh. <br /><br />
+Sales: <a href="tel:+8801977936623">+8801977936623</a> <br />
+Available during 10:00 AM to 06:00 PM (GMT +06:00)<br /><br />
+
+WebSite: <a href="http://www.xeonbd.com/">www.xeonbd.com</a> | Support: <a href="http://support.xeonbd.com/">support.xeonbd.com</a><br />
+Query: <a href="http://query.xeonbd.com/">query.xeonbd.com</a> | Blog: <a href="http://blog.xeonbd.com/">blog.xeonbd.com</a><br />
+</h2>
+			</div>
+
+			<!-- Social icons -->
+			<div class="social-icons">
+
+				<a href="https://www.facebook.com/myxeonbd/"><i class="fa fa-facebook"></i></a>
+				<a href="http://www.twitter.com/xeonbd"><i class="fa fa-twitter"></i></a>
+				<a href="https://plus.google.com/107510523852147662268/posts"><i class="fa fa-google-plus"></i></a>
+				<a href="https://www.linkedin.com/company/xeonbd"><i class="fa fa-linkedin"></i></a>
+
+			</div>
+
+		</section>
+		<!-- END - Home/Left Part -->
+
+	<!-- ///////////////////\\\\\\\\\\\\\\\\\\\ -->
+    <!-- ********** Resources jQuery ********** -->
+    <!-- \\\\\\\\\\\\\\\\\\\/////////////////// -->
+	
+	<!-- * Libraries jQuery, Easing and Bootstrap - Be careful to not remove them * -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.easings.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- PhotoSwipe Core JS file -->
+	<script src="js/velocity.min.js"></script> 
+
+	<!-- PhotoSwipe UI JS file -->
+	<script src="js/velocity.ui.min.js"></script> 
+
+	<!-- Newsletter plugin -->
+	<script src="js/notifyMe.js"></script>
+
+	<!-- Slideshow/Image plugin -->
+	<script src="js/vegas.js"></script>
+
+	<!-- Scroll plugin -->
+	<script src="js/jquery.mousewheel.js"></script>
+
+	<!-- Custom Scrollbar plugin -->
+	<script src="js/jquery.mCustomScrollbar.js"></script>
+
+	<!-- Popup Newsletter Form -->
+	<script src="js/classie.js"></script>
+	<script src="js/dialogFx.js"></script>
+
+	<!-- Countdown plugin -->
+	<script src="js/jquery.countdown.js"></script>
+
+	<script>
+	$("#getting-started")
+		// Year/Month/Day Hour:Minute:Second
+		.countdown("2016/10/24 15:30:30", function(event) {
+			$(this).text(
+				event.strftime('%D Days %Hh %Mm %Ss')
+			);
+	});
+	</script>
+
+	<!-- Main JS File -->
+	<script src="js/main.js"></script>
+	
+	<!--[if lt IE 10]><script type="text/javascript" src="js/placeholder.js"></script><![endif]-->
+
+	</body>
+
+</html>
