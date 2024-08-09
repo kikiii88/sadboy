c920169893c8bbc8f7dab1bdf84b908b2ffc927e
Kiki <kikiamalia122000@gmail.com>
2024-08-09 17:55:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
-  <loc>https://richmond-furniture.co.uk/</loc>
-</url>
-</urlset>
+User-Agent: *
+Allow: /index.php
+Sitemap: https://richmond-furniture.co.uk/sitemap.xml
